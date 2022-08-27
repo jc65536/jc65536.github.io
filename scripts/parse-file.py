476d92a4b6cd822f61aabe7d72ff599aaee77cfa
Jason Cheng <jasoncheng224@gmail.com>
2022-08-27 01:39:19 -0700

    Python destination implementation

--- scripts/parse-file.py
+++ scripts/parse-file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from io import StringIO, TextIOBase
 import re
 import sys
 from typing import Iterable, Iterator
@@ -9,35 +10,21 @@ TEMPLATE_DIR = "templates"
 DIRECTIVE_PAT = re.compile("{([^}]+)}")
 OPEN_PAT = re.compile("{(\**)")
 
-remainder = ""
 
-
-def join_strip(lines: Iterable[str]):
-    return "".join(lines).strip()
-
-
-def get_rem():
-    return remainder
-
-
-def set_rem(s: str):
-    global remainder
-    remainder = s
-
-
-def comb_rem(src: Iterator[str]):
-    if remainder:
-        yield remainder
+def comb_rem(rem: str, src: Iterator[str]):
+    if rem:
+        yield rem
     for line in src:
         yield line
 
 
-def parse_file(src: Iterator[str], subs: dict[str, str] = {},
-               exit_pat: str = None):
+def parse_file(src: Iterator[str], dest: TextIOBase = sys.stdout,
+               subs: dict[str, str] = {}, exit_pat: str = None,
+               rem: str = None):
 
     exit_found = False
 
-    for line in comb_rem(src):
+    for line in comb_rem(rem, src):
         # Breaking this while implies that we've consumed the whole line
         while line:
             if exit_pat:
@@ -47,43 +34,42 @@ def parse_file(src: Iterator[str], subs: dict[str, str] = {},
             match = DIRECTIVE_PAT.search(line)
 
             if not exit_found and not match:
-                yield line
+                dest.write(line)
                 break
 
             if exit_found and not (match and match.start() < exit_idx):
-                yield line[:exit_idx]
-                set_rem(line[exit_idx + len(exit_pat):])
-                return
+                dest.write(line[:exit_idx])
+                dest.flush()
+                return line[exit_idx + len(exit_pat):]
 
             if match.start() > 0:
-                yield line[:match.start()]
+                dest.write(line[:match.start()])
 
             line = line[match.end():]
 
             match match.group(1).split():
                 case ["include", filename]:
                     with open(f"{TEMPLATE_DIR}/{filename}") as file:
-                        yield from parse_file(file)
+                        parse_file(file, dest)
 
                 case ["include", filename, "with"]:
-                    incl_subs = {}
-                    parse_subs(src, incl_subs, subs)
-                    line = get_rem()
+                    incl_subs: dict[str, str] = {}
+                    line = parse_subs(src, incl_subs, subs, line)
 
                     with open(f"{TEMPLATE_DIR}/{filename}") as file:
-                        yield from parse_file(file, incl_subs)
+                        parse_file(file, dest, incl_subs)
 
                 case [key] if key in subs:
-                    yield subs[key]
+                    dest.write(subs[key])
 
                 case _:
-                    yield match[0]
+                    dest.write(match[0])
+    dest.flush()
 
 
 def parse_subs(src: Iterator[str], incl_subs: dict[str, str],
-               subs: dict[str, str]):
-
-    for line in comb_rem(src):
+               subs: dict[str, str], rem: str):
+    for line in comb_rem(rem, src):
         while line:
             exit_idx = line.find("{endwith}")
             exit_found = exit_idx != -1
@@ -95,23 +81,21 @@ def parse_subs(src: Iterator[str], incl_subs: dict[str, str],
                 break
 
             if exit_found and not (colon_found and colon_idx < exit_idx):
-                set_rem(line[exit_idx + len("{endwith}"):])
-                return
+                return line[exit_idx + len("{endwith}"):]
 
             key = line[:colon_idx].strip()
             match = OPEN_PAT.search(line, colon_idx + 1)
+            strbuf = StringIO()
 
             if match:
-                set_rem(line[match.end():])
-                incl_subs[key] = join_strip(
-                    parse_file(src, subs, match[1] + "}"))
-                line = get_rem()
+                line = parse_file(src, strbuf, subs, match[1] + "}",
+                                  line[match.end():])
             else:
-                incl_subs[key] = join_strip(parse_file(
-                    iter([line[colon_idx + 1:]]), subs))
-                break
+                parse_file(iter([line[colon_idx + 1:]]), strbuf, subs)
+                line = ""   # Force exit
+
+            incl_subs[key] = strbuf.getvalue().strip()
 
 
 with open(sys.argv[1]) as file:
-    for line in parse_file(file):
-        print(line, end="")
+    parse_file(file)

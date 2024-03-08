7a68345725b67d4b072d8b1f622a3c2a57db5e1a
Jason Cheng <jasoncheng224@gmail.com>
2024-03-08 01:23:29 -0800

    Remove eval pages

--- html/demos/physics-1c_files/config.js
+++
@@ -1,3 +0,0 @@
-//>>built
-var dojoConfig={async:!0,cacheBust:"v\x3d9",baseUrl:"scripts/lib/",parseOnLoad:!1,isDebug:!1,packages:["ep_online","dijit","dojo","dojox"],selectorEngine:"lite",tlmSiblingOfDojo:!1};
-//# sourceMappingURL=config.js.map
